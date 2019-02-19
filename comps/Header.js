195bc121115f87a6004a9a8f36b62cfcb0533a0a
vipul-bravvura <vipul.s@bravvura.in>
2019-02-19 23:26:32 +0530

    'first'

---
+++ comps/Header.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+const linkStyle = {
+    marginRight:15
+}
+
+const Header = () => ( 
+    <div>
+        <Link href="/">
+         <a>Home </a>
+        </Link>
+         
+        <Link href="/about">
+         <a> About</a>
+        </Link>
+    </div>
+)
+
+export default Header
---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Layout from "../comps/MyLayout";
+
+const About = () => (
+    <Layout>
+        <p>Hellow About</p>
+    </Layout>
+)
+
+export default About
